3e7379c53ef3b03fcadf006220f52b83eae5b441
wuleiyuan <wuly@youyuan.com>
2026-02-28 00:18:36 +0800

    Fix: use valid maptiler styles

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -232,8 +232,8 @@ export const SWIMMING_COLOR = 'rgb(255,51,51)';
 export const MAP_TILE_VENDOR = 'maptiler';
 
 // map tiles style name, see MAP_TILE_STYLES for more details
-export const MAP_TILE_STYLE_LIGHT = 'osm-bright';
-export const MAP_TILE_STYLE_DARK = 'dark-matter';
+export const MAP_TILE_STYLE_LIGHT = 'streets-light';
+export const MAP_TILE_STYLE_DARK = 'streets-dark';
 
 // access token. you can apply a new one, it's free.
 // maptiler: Gt5R0jT8tuIYxW6sNrAg | sign up at https://cloud.maptiler.com/auth/widget
