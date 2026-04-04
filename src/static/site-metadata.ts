a0d1a6f2d1845db60c3469232ded7ec2c3e33fab
wuleiyuan <wuly@youyuan.com>
2026-04-04 15:56:24 +0800

    fix: update nav links to user profile

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -15,8 +15,8 @@ const getBasePath = () => {
 };
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
+  siteTitle: 'WuLeiYuan Running Page',
+  siteUrl: 'https://running-page-v2.vercel.app',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
   navLinks: [
@@ -26,11 +26,11 @@ const data: ISiteMetadataResult = {
     },
     {
       name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
+      url: 'https://github.com/wuleiyuan',
     },
     {
       name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+      url: 'https://github.com/wuleiyuan/running-page-v2',
     },
   ],
 };
